0975e81996c20becc46eab6a7788f2302e0c7781
miseljacobSNOW <126715541+miseljacobSNOW@users.noreply.github.com>
2023-03-02 12:10:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,26 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+        return my_cur.fetchall()
+    
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+
+if streamlit.button('Get Fruit Load List);
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -26,10 +42,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #displays our table
 streamlit.dataframe(fruits_to_show)
 
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
+
  
 streamlit.header('Fruityvice Fruit Advice!')
 try:
@@ -43,7 +56,6 @@ except URLError as e:
    streamlit.error()
       
 streamlit.text("Hello from Snowflake:")
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+
 
 add_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
